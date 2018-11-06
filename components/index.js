6943e86e5a772cebacced5fd35a6e0d8a611eea6
xiaoshuang.li <qq1157502759@gmail.com>
2018-11-06 10:57:24 +0800

    change module and Bundle & delete Animate and Loading 2.0

--- components/index.js
+++ components/index.js
@@ -1,17 +1,13 @@
 const Normal = require('./Normal');
-const Animate = require('./Animate');
 const Bundle = require('./Bundle');
 const Iconfont = require('./Iconfont');
-const Loading = require('./Loading');
 const Raven = require('./Raven');
 const RelativeRouter = require('./RelativeRouter');
 
 module.exports = {
   Normal,
-  Animate,
   Bundle,
   Iconfont,
-  Loading,
   Raven,
   RelativeRouter,
 };
